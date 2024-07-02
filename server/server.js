7f7e7838d64cbb3af99ca92c5b9be9bc2c2ec0e5
Leland Byrd <lbyrd@truecoders.io>
2024-07-01 19:09:13 -0500

    added image resources for background of home and battle page

--- server/server.js
+++ server/server.js
@@ -2,3 +2,8 @@ const express = require("express");
 const path = require("path");
 
 const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
