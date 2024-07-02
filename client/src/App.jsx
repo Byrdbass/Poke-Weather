855249c5bff8bb7110ce3f30af9e0104f3027e03
Leland Byrd <lbyrd@truecoders.io>
2024-07-01 19:23:57 -0500

    made background image properties in app.css - still cutoff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
+import "./app.css"
 import { useState } from 'react'
 
 function App() {
 
   return (
-    <div>
-
+    <div className="background-home">
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, amet autem harum debitis cum illum, esse ab fugiat asperiores voluptatibus dolores excepturi natus culpa architecto praesentium maiores itaque quod corrupti?
 
     </div>
 
