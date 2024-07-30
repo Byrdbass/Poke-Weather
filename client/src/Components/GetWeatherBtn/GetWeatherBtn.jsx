98f6bc11a0018d2bd0b50c2a7d95dc7473420bca
byrdbass <byrdbass@gmail.com>
2024-07-30 09:08:30 -0500

    successfully added dotenv and api key for fetching weather

--- client/src/Components/GetWeatherBtn/GetWeatherBtn.jsx
+++ client/src/Components/GetWeatherBtn/GetWeatherBtn.jsx
@@ -1,10 +1,12 @@
 import './get-weather-btn.css';
+import getCityWeather from '../../Helpers/fetchWeather';
 
 export default function GetWeatherBtn() {
     return (
         <div className="weather-btn-outer-div">
             <div className="weather-btn-inner-div">
-                <button className="weather-btn">
+                {/* AFTER WEATHER IS PLACED IN SEARCH FIELD, CHANGE THIS FUNCTION */}
+                <button className="weather-btn" onClick={getCityWeather}>
                     Fetch Weather
                 </button>
             </div>
---
+++ client/src/Helpers/fetchWeather.js
@@ -0,0 +1,38 @@
+const apiKey = import.meta.env.VITE_WEATHER_API_KEY || process.env.VITE_WEATHER_API_KEY;
+export default function getCityWeather() {
+    //TODO - default city name to grabbing someone's location
+    let cityName = 'austin';
+    let latitude = '';
+    let longitude = '';
+    let recentSearches = [];
+    //TODO: make this a string literal
+    var url = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + apiKey
+    fetch(url)
+    .then(function (response) {
+            console.log(response);
+                return response.json();
+            })
+            .then(function (data) {
+                console.log("this is current weather", data);
+                // let iconData = data.weather[0].icon;
+            //     var iconImg = $('<img>');
+            //     $('#iconImg').addClass("card.small left")
+            //     $('#iconImg').attr('src', 'https://openweathermap.org/img/wn/' + iconData + '@4x.png')
+            //     $('#icon').append(iconImg)
+            //     var temp = data.main.temp
+            //     $('#temp').text(temp + " degrees")
+            //     var windSpeed = data.wind.speed
+            //     $('#windSpeed').text(windSpeed + " is the Wind Speed")
+            //    console.log(data.name);
+            //     latitude = data.coord.lat;
+            //     longitude = data.coord.lon;
+            //     console.log("the latitude is " + latitude, "the longitude is " + longitude);
+            //     getForecastUvi(latitude, longitude);
+            //     $('#date').text(moment().format('dddd, MM/DD/YYYY'))
+            //     add the date for the next five days?  using moment or weather API?
+            //     save the cityName to local storage
+            //     localStorage.setItem("cityName", data.name)
+            //     pass the coordinates to other url call for Lat and lon??
+            return data;
+            });
+}
