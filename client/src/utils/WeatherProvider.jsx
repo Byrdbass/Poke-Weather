e74d5188acaa8b51a422cda1cc1dad95a2d97e93
byrdbass <byrdbass@gmail.com>
2024-08-02 15:26:25 -0500

    attempt to modularize fetch calls to different weather apis based off of input

--- client/src/utils/WeatherProvider.jsx
+++ client/src/utils/WeatherProvider.jsx
@@ -11,64 +11,97 @@ export const WeatherProvider = ({ children }) => {
 
     const [location, setLocation] = useState(null);
     const [weatherToday, setWeatherToday] = useState({
-        url: 'https://api.openweathermap.org/data/2.5/weather?q=', //+ cityName + '&units=imperial&appid=' + apiKey,
+        urlByCity: 'https://api.openweathermap.org/data/2.5/weather?q=',
+        urlOneCall: 'https://api.openweathermap.org/data/2.5/onecall?lat=',
         //TODO - default city name to grabbing someone's location
-        cityName: "austin",
+        cityName: "new york",
         iconSrc: "",
         temp: undefined,
         windSpeed: '',
         lat: '',
         lon: '',
-        
+
     })
 
+    //ON PAGE LOAD
     useEffect(() => {
-        if (navigator.geolocation){
+        if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(success, error)
         }
-        else{
-            setLocation(weatherToday.cityName)
+        else {
+            console.log("Geolocation not supported")
+            fetch(`${weatherToday.urlByCity}${weatherToday.cityName}&units=imperial&appid=${apiKey}`)
+            .then(function (response) {
+                // console.log(response);
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data)
+                setWeatherToday(prev => ({
+                    ...prev,
+                    iconSrc: `https://openweathermap.org/img/wn/${data.weather[0]?.icon}@4x.png`,
+                    temp: data.main.temp
+                }))
+            })
         }
         function success(location){
-            console.log(location)
+            setWeatherToday(prev => ({
+                ...prev,
+                lat: location.coords.latitude,
+                lon: location.coords.longitude
+            }))
+
         }
         function error() {
             console.log("Unable to retrieve your location");
-          }
+            fetch(`${weatherToday.urlByCity}${weatherToday.cityName}&units=imperial&appid=${apiKey}`)
+            .then(function (response) {
+                // console.log(response);
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data)
+                setWeatherToday(prev => ({
+                    ...prev,
+                    iconSrc: `https://openweathermap.org/img/wn/${data.weather[0]?.icon}@4x.png`,
+                    temp: data.main.temp
+                }))
+            })
+        }
     }, [])
 
 
-    //TODO : try to fit in other api call here 
-    useEffect(() => {
-        fetch(`${weatherToday.url}${weatherToday.cityName}&units=imperial&appid=${apiKey}`)
-        .then(function (response) {
-            // console.log(response);
-            return response.json();
-        })
-        .then( (data) => {
-            console.log(data)
-            setWeatherToday(prev => ({
-                ...prev,
-                iconSrc: `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`,
-                temp: data.main.temp
-            }))
-        })
-    }, [weatherToday.cityName])
+    // //TODO : try to fit in other api call here 
+    // useEffect(() => {
+    //     fetch(`${weatherToday.urlByCity}${weatherToday.cityName}&units=imperial&appid=${apiKey}`)
+    //     .then(function (response) {
+    //         // console.log(response);
+    //         return response.json();
+    //     })
+    //     .then((data) => {
+    //         console.log(data)
+    //         setWeatherToday(prev => ({
+    //             ...prev,
+    //             // iconSrc: `https://openweathermap.org/img/wn/${data.weather[0]?.icon}@4x.png`,
+    //             temp: data.main.temp
+    //         }))
+    //     })
+    // }, [weatherToday.cityName])
 
     //FUNCTIONS TO UPDATE PROVIDER VALUES
     const updateCityName = (newCity) => {
         setWeatherToday(prev => ({
-            ...prev, 
+            ...prev,
             cityName: newCity
         }))
 
     }
 
 
-    return <WeatherContext.Provider value = {{
+    return <WeatherContext.Provider value={{
         weatherToday, setWeatherToday,
         updateCityName
     }}>
         {children}
-    </WeatherContext.Provider> 
+    </WeatherContext.Provider>
 }
